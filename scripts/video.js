c664478be41ba541064f586909f02a1a23e4cfc2
Arpita Das Sara <das021400@gmail.com>
2025-10-17 19:22:04 +0600

    added new text

--- scripts/video.js
+++ scripts/video.js
@@ -9,6 +9,68 @@ const loadCategories = () => {
     .then(data => displayCategories(data.categories))
     .catch(error => console.log(error));
 };
+const loadVideos = () => {
+   //fetch the data
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(res => res.json())
+    .then(data => displayVideos(data.videos))
+    .catch(error => console.log(error));
+};
+ // const cardDemo={
+//  category_id: "1001",
+//  video_id: "aaad",
+//    thumbnail: "https://i.ibb.co/f9FBQwz/smells.jpg",
+//    title: "Smells Like Teen Spirit",
+//    authors: [
+//        {
+//           profile_picture: "https://i.ibb.co/k4tkc42/oliviar-harris.jpg",
+//            profile_name: "Oliver Harris",
+//            verified: true
+//        }
+//    ],
+ //  others: {
+//        views: "5.4K",
+//        posted_date: "1672656000"
+//    }
+//    description: 
+//    "'Smells Like Teen Spirit' by Oliver Harris captures the raw energy and rebellious spirit of youth. With over 5.4K views, this track brings a grunge rock vibe, featuring powerful guitar riffs and compelling vocals. Oliver's verified profile guarantees a quality musical journey that resonates with fans of dynamic, high-energy performances."
+// };
+
+
+const displayVideos = (videos) => {
+    const videoContainer=document.getElementById("videos");
+    videos.forEach((video) => {
+        console.log(video);
+        const card= document.createElement('div');
+        card.classList="card card-compact";
+        card.innerHTML=`
+        <figure class="h-[200px]">
+    <img
+      src="${video.thumbnail}
+      class="w-full h-full object-cover"
+      alt="Shoes" />
+  </figure>
+  <div class="px-0 py-2 flex gap-2">
+    <div>
+      <img class="w-1- h-10 rounded-full object-cover" src=${video.authors[0].profile_picture} />
+      <div>
+      <h2 class "font-bold">${video.title}</h2>
+      <div><p></p>
+      </div>
+      <p class="text-gray-400">${video.authors[0].profile_name}</p>
+      <img src="https://cdn-icons-png.flaticon.com/128/7641/7641727.png">
+      </div>
+    </div>
+  
+  
+    </div>
+  </div>
+
+   
+        `;
+        videoContainer.append(card);
+    });
+};
 
 //{
 //"category_id": "1001",
@@ -31,3 +93,4 @@ const displayCategories = (categories) => {
     });
 };
 loadCategories();
+loadVideos();
